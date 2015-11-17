7b94193ab09fcc11e36aeaf910b64a24f14f58a1
lindsay stevens <lindsay.stevens.au@gmail.com>
2015-11-18 09:14:12 +1100

    add httpd option for "insecure" local http only behaviour

--- docker/httpd/httpd/conf/docker-entrypoint.sh
+++ docker/httpd/httpd/conf/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# If the HTTPD_INSECURE environment variable is set, then define the flag for it.
+if [ "$HTTPD_INSECURE" == "yes" ]; then
+    define_insecure = "-DInsecure"
+fi
+
 if [ "$*" = 'httpd -DFOREGROUND' ]; then
     # Apache gets grumpy about PID files pre-existing
     rm -f /usr/local/apache2/logs/httpd.pid
@@ -11,7 +16,7 @@ if [ "$*" = 'httpd -DFOREGROUND' ]; then
     chmod g+s $HTTPD_PREFIX
     chmod -R 775 $HTTPD_PREFIX
     
-    exec gosu httpd "$@"
+    exec gosu httpd "$@" $define_insecure
 fi
 
 exec "$@"
